01580954a6e5380bfd986672d94f96ce50cbf9a8
zhanglu <comely_zl@163>
2020-06-28 20:18:02 +0800

    commit code

--- rpc-netty/src/main/java/com/beinglee/rpc/transport/netty/ResponseInvocation.java
+++ rpc-netty/src/main/java/com/beinglee/rpc/transport/netty/ResponseInvocation.java
@@ -1,15 +1,18 @@
 package com.beinglee.rpc.transport.netty;
 
 import com.beinglee.rpc.transport.InFlightRequests;
+import com.beinglee.rpc.transport.ResponseFuture;
 import com.beinglee.rpc.transport.command.Command;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author zhanglu
  * @date 2020/6/20 15:08
  */
+@Slf4j
 @ChannelHandler.Sharable
 public class ResponseInvocation extends SimpleChannelInboundHandler<Command> {
 
@@ -20,7 +23,21 @@ public class ResponseInvocation extends SimpleChannelInboundHandler<Command> {
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Command command) {
+    protected void channelRead0(ChannelHandlerContext ctx, Command response) {
+        ResponseFuture future = inFlightRequests.remove(response.getHeader().getRequestId());
+        if (future != null) {
+            future.getFuture().complete(response);
+        } else {
+            log.warn("Drop response : {}", response);
+        }
+    }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.warn("ResponseInvocation occur error!", cause);
+        super.exceptionCaught(ctx, cause);
+        if (ctx.channel().isActive()) {
+            ctx.channel().close();
+        }
     }
 }
